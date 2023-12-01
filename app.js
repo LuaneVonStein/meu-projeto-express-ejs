aa731d3c3b7da0608f34032737ea08a4b39fe96b
LuaneVonStein <luanevonstein62@gmail.com>
2023-12-01 13:49:16 -0400

    commit

--- app.js
+++ app.js
@@ -6,17 +6,16 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 const produtos = [
-  {id: 1, nome: "colares de prata", preco: "300", descricao: "colares lisos", imagem: "produto.jpg"},
-  {id: 2, nome: "colares de ouro", preco: "500", descricao: "colares trançados", imagem: "produto.jpg"},
+  {id: 1, nome: "colares de prata", preco: "300", descricao: "colares lisos", imagem: "lMG/anel.4.jpg"},
+  {id: 2, nome: "colares de ouro", preco: "500", descricao: "colares trançados", imagem: "IMG/anel.dv.jpg"},
   {id: 3, nome: "colares de ouro branco", preco: "250", descricao: "colares de ouro branco lisos e trançados uma das pedras mais iluminadas", imagem: "produto.jpg"},
-  {id: 4, nome: "pulseiras", preco: "300", descricao: "pulseiras pratas ", imagem: "produto.jpg"},
-  {id: 5, nome: "anel", preco: "300", descricao: "anel de ouro", imagem: "produto.jpg"},
-  {id: 6, nome: "conjuntos de ouro e esmeralda", preco: "1500", descricao: "conjunto de brinco,colar e anel", imagem: "produto.jpg"},
-  {id: 7, nome: "braceletes", preco: "400", descricao: "bracelete de prata", imagem: "produto.jpg"},
-  {id: 8, nome: "alianças de pratas", preco: "400", descricao: "conjuto com tres aalianças pratas", imagem: "produto.jpg"},
-  {id: 9, nome: "nome do produto", preco: "400", descricao: "descricao do produto", imagem: "produto.jpg"},
-  {id: 10, nome: "nome do produto", preco: "400", descricao: "descricao do produto", imagem: "produto.jpg"},
-
+  {id: 4, nome: "pulseiras", preco: "300", descricao: "pulseiras pratas ", imagem: "IMG/colar.de.cruz.jpg"},
+  {id: 5, nome: "anel", preco: "300", descricao: "anel de ouro", imagem: "IMG/esmeralda.3.jpg"},
+  {id: 6, nome: "conjuntos de ouro e esmeralda", preco: "1500", descricao: "conjunto de brinco,colar e anel", imagem: "IMG/esmeralda.jpg"},
+  {id: 7, nome: "braceletes", preco: "400", descricao: "bracelete de prata", imagem: "IMG/images.jpg"},
+  {id: 8, nome: "alianças de pratas", preco: "400", descricao: "conjuto com tres aalianças pratas", imagem: "IMG/images3.jpg"},
+  {id: 9, nome: "nome do produto", preco: "400", descricao: "descricao do produto", imagem: "IMG/imagesp.jpg"},
+  {id: 10, nome: "nome do produto", preco: "400", descricao: "descricao do produto", imagem: "IMG/prata.2.jpg"}
 ]
 
 function buscarProdutoPorId(id){
